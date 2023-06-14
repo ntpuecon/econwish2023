19b69220043d1217cbb9507e22f2f8453e3181d6
Martin老師 <tpemartin@gmail.com>
2023-06-14 09:08:12 +0800

    update

--- Finance.R
+++
@@ -1,7 +0,0 @@
-googlesheets4::read_sheet(
-    "https://docs.google.com/spreadsheets/d/18WNH5ckkq1a-iPpGREjwgyW3Cbb51YZ6qqFGyyrNfD8/edit#gid=1991794251",
-    "ValidList"
-) -> sh
-
-sh |>
- dplyr::filter()
